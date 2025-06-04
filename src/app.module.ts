4d89e389a7dc1c32929464aa86700a30b113a9dc
Wirbelwind <komendantov2580@gmail.com>
2025-06-04 14:54:12 +0300

    apply config module as global

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlertsModule } from './alerts/alerts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AlertsModule],
+  imports: [
+    AlertsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
