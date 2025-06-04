c94aa772a96776041450d3a01672518f1587a0d6
Wirbelwind <komendantov2580@gmail.com>
2025-06-04 14:37:55 +0300

    create controller alerts

--- src/alerts/alerts.module.ts
+++ src/alerts/alerts.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AlertsService } from './alerts.service';
+import { AlertsController } from './alerts.controller';
 
 @Module({
-  providers: [AlertsService]
+  providers: [AlertsService],
+  controllers: [AlertsController]
 })
 export class AlertsModule {}
